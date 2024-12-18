c5e35c1cffbdabbd88f0d4f6277b81da3bca3b42
Juan Miguel Dumagpi <144773443+JuanDumagpi@users.noreply.github.com>
2024-12-18 20:05:42 +0400

    Add files via upload
    First commit, testing how to append posts from an array, and making adding comments work.
    Comments aren't working still, that would be for the next commit.

---
+++ Assesment 2/data.js
@@ -0,0 +1,36 @@
+var posts = [
+    {
+        id: 1,
+        title: "This post is about dogs",
+        author: "Jane",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "John",
+                content: "This is a comment."
+            },
+            {
+                author: "Jack",
+                content: "This is also a comment."
+            }
+        ]
+    },
+    {   
+        id:2,
+        title: "This post is not about dogs",
+        author: "Not Jane",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Not John",
+                content: "This is a comment."
+            },
+            {
+                author: "Not Jack",
+                content: "This is also a comment."
+            }
+        ]
+    }
+]
